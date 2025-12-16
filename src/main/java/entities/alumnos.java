deca079dac233c726335b0e4939405efa7fab7be
melan <lemapilapana.melanie@jgvaldemora.org>
2025-12-16 23:39:24 +0100

    trabajo finalizado

---
+++ src/main/java/entities/alumnos.java
@@ -0,0 +1,105 @@
+package entities;
+//aqui estan los atributos privados
+
+public class alumnos {
+    private  String nombre;
+    private String apellido;
+    private int edad;
+    private String curso;
+    private String dni;
+    private String email;
+    private String telefono;
+    private double notamedia;
+ // constructor  vacio
+    public alumnos() {
+    }
+ // constructor con conparametros
+    public alumnos(String nombre, String apellido, int edad, String curso,
+                   String dni, String email,String telefono, double notamedia) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+        this.curso = curso;
+        this.dni = dni;
+        this.email = email;
+        this.telefono = telefono;
+        this.notamedia = notamedia;
+    }
+ // getters y setters
+    public String getNombre(){
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getCurso() {
+        return curso;
+    }
+
+    public void setCurso(String curso) {
+        this.curso = curso;
+    }
+
+    public String getDni() {
+        return dni;
+    }
+
+    public void setDni(String dni) {
+        this.dni = dni;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public double getNotamedia() {
+        return notamedia;
+    }
+
+    public void setNotamedia(double notamedia) {
+        this.notamedia = notamedia;
+    }
+
+    // 2 metodos estaticos
+      public static boolean esMayorDeEdad() {
+          return esMayorDeEdad(0);
+      }
+
+    // 2 metodos estaticos
+    public static boolean esMayorDeEdad(int edad) {
+        return edad >= 18;
+    }
+    public static boolean notaAprobada(double notamedia) {
+        return notamedia >= 5.0;
+    }
+}

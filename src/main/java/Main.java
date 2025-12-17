68e292a739c20c6e1a318a521d9d2ca5dd305092
melan <lemapilapana.melanie@jgvaldemora.org>
2025-12-17 09:30:02 +0100

    trabajo Arreglado y finalizado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,8 @@
-import entities.Alumnos;
-import entities.Alumnos;
+import entities.alumnos;
 
 public class Main {
     public static void main(String[] args) {
-        Alumnos  alumno1 = new Alumnos();
+        alumnos  alumno1 = new alumnos ();
         alumno1.setNombre("Olivia");
         alumno1.setApellido("Perez");
         alumno1.setEdad(20);
@@ -21,16 +20,7 @@ public class Main {
         System.out.println("Telefono: " + alumno1.getTelefono());
         System.out.println("Nota Media: " + alumno1.getNotamedia());
       // constructor con todos los parametros
-        Alumnos  alumno2 = new Alumnos (
-                "Liam",
-                "Garcia",
-                22,
-                "1º DAW",
-                "7894561B",
-                "liam.garcia@email.com",
-                "612345678",
-                9.0
-        );
+        alumnos  alumno2 = new alumnos ("Liam", "Garcia", 22, "1º DAW", "7894561B","liam.garcia@email.com","612345678", 9.0);
      // metodos estaticos
         System.out.println("Nombre: " + alumno2.getNombre());
         System.out.println("Apellido: " + alumno2.getApellido());
@@ -40,14 +30,5 @@ public class Main {
         System.out.println("Email: " + alumno2.getEmail());
         System.out.println("Telefono: " + alumno2.getTelefono());
         System.out.println("Nota Media: " + alumno2.getNotamedia());
-
-
-        System.out.println("¿Alumno1 mayor de edad? " +
-                Alumnos.esMayorDeEdad(alumno1.getEdad()));
-
-        System.out.println("¿Alumno1 aprobado? " +
-                Alumnos.notaAprobada(alumno1.getNotamedia()));
-
     }
-
 }
